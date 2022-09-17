170c9917f0f75aea5889e82f4e0bb77a51ea56d4
Clara <claradsfcarneiro@gmail.com>
2022-09-17 18:52:41 -0300

    Realizar exercício 1

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,3 +4,21 @@ const objeto = {
     username: "astrodev_labenu",
     senha: "melhorDeTodos"
 }
+
+const caixaAlta = function(obj) {
+    Object.keys(obj).forEach((key) => {
+        obj[key] = obj[key].toUpperCase();
+    });
+    return obj;
+}
+
+const textoCorrido = function(obj) {
+    return `${obj.nome}, ${obj.profissao}, ${obj.username}, ${obj.senha}`
+}
+
+function caixaAltaOuCorrrido(obj, funcao) {
+    console.log(funcao(obj));
+}
+
+caixaAltaOuCorrrido(objeto, caixaAlta);
+caixaAltaOuCorrrido(objeto, textoCorrido);
